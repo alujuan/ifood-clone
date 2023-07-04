fc67d14a5ba17e396392edef78e3c132fd6e4bb2
alujuan <juanpeterssonb@gmail.com>
2023-07-04 15:13:12 -0300

    instalação de dependências

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import { ScrollView, StyleSheet } from 'react-native';
-import Endereco from "../components/Home/Endereco';
-import Input from "../components/Input';
-import CupomDesconto from "../components/Home/CupomDesconto';
-import Sugestoes from "../components/Home/Sugestoes';
-import Promocoes from "../components/Home/Promocoes';
-import Ofertas from "../components/Home/Ofertas';
-import Categorias from "../components/Home/Categorias';
-import Restaurantes from "../components/Home/Restaurantes';
+import { ScrollView, StyleSheet } from "react-native";
+import Endereco from "../components/Home/Endereco";
+import Input from "../components/Input";
+import CupomDesconto from "../components/Home/CupomDesconto";
+import Sugestoes from "../components/Home/Sugestoes";
+import Promocoes from "../components/Home/Promocoes";
+import Ofertas from "../components/Home/Ofertas";
+import Categorias from "../components/Home/Categorias";
+import Restaurantes from "../components/Home/Restaurantes";
 
 export default function Home() {
   return (
@@ -28,6 +28,6 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
